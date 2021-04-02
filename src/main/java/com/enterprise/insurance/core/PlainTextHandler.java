0f5816feaed4b481f7fa5a98a07eaa3f2406d691
Florian Bauer <bauerflorian13@gmx.de>
2021-04-02 23:55:32 +0200

    Add simple routing with undertow

---
+++ src/main/java/com/enterprise/insurance/core/PlainTextHandler.java
@@ -0,0 +1,20 @@
+package com.enterprise.insurance.core;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class PlainTextHandler implements HttpHandler {
+
+    private final String value;
+
+    public PlainTextHandler(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void handleRequest(HttpServerExchange exchange) {
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+        exchange.getResponseSender().send(value + "\n");
+    }
+}
